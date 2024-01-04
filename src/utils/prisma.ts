ad928c784807b85965b3955662bae9e168948828
Anish Tilloo <anishtilloo@gmail.com>
2024-01-05 00:00:55 +0530

    updated the auth schema in prisma schema

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
+
+// First we are creating a variable of type PrismaClient
+let prisma: PrismaClient;
+
+
+// So this declar global is used to declare something in a global scope
+declare global {
+    // here we are declearing var __prisma as a global variable
+    var __prisma: PrismaClient | undefined;
+}
+
+// here we are checking if the global prisma variabel does have a value or not if not then we are assigning a new instance of prisma client
+if(!global.__prisma) {
+    global.__prisma = new PrismaClient();
+}
+
 // created an instance of Prisma client
-const prisma = new PrismaClient();
+// here we are passing the value of global prisma variable to the variable we declared above
+prisma = global.__prisma;
+
 
 export default prisma;
