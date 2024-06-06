cbba559361f00c41543bb46ad6e8bf09a8580167
Anish Tilloo <anish.tilloo@claimgenius.com>
2024-06-06 17:23:28 +0530

    updated the error message for validation middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -16,7 +16,7 @@ export const validate =
       if (error instanceof ZodError) {
         return res.status(400).json({
           success: false,
-          message: "Validation of the data from the user failed",
+          message: `Validation of the data from the user failed -> Error Path => ${error.errors[0].path} -> ${error.errors[0].message}`,
           errors: error.errors,
         });
       }
