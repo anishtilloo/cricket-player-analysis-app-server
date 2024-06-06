203f9e19815096c0e50a4fd8ed2dbbeec00f6472
Anish Tilloo <anish.tilloo@claimgenius.com>
2024-06-06 17:22:50 +0530

    added necessary check and refactored the code for the team post controller

--- src/controllers/teams/team.post.controller.ts
+++ src/controllers/teams/team.post.controller.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from 'express';
-import addTeamService  from "../../services/teams/team.post.services"
 import httpStatus from 'http-status';
+
+import addTeamService  from "../../services/teams/team.post.services"
+import { getTeamUsingName } from '../../services/teams/team.get.services';
+import ApiError from '../../utils/ApiError';
 
 // POST request
 // create new team in the DB
 export async function addTeam(req: Request , res: Response) {
     try {
         const team = req.body;
+        const teamExist = await getTeamUsingName(team.name);
+        
+        if (teamExist) {
+            throw new ApiError(httpStatus.BAD_REQUEST, "Please select a unique name for your team");
+        }
         const result = await addTeamService(team);
+        
         res.status(httpStatus.CREATED).json({ 
             success: true, 
             message: "Team Added Successfully", 
@@ -15,8 +24,8 @@ export async function addTeam(req: Request , res: Response) {
         })
     } catch (error) {
         res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ 
-            success: true, 
-            message: "Something went wrong", 
+            success: false, 
+            message: `Something went wrong -> ${error}`, 
             error: error 
         })
     }
