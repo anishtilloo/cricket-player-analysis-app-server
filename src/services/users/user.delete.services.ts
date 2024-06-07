8bb1fa8061e227d57900120abb5f2e73ece1b735
Anish Tilloo <anish.tilloo@claimgenius.com>
2024-06-07 13:57:35 +0530

    refactored the code for the delete user service

--- src/services/users/user.delete.services.ts
+++ src/services/users/user.delete.services.ts
@@ -1,13 +1,6 @@
-import httpStatus from "http-status";
-import ApiError from "../../utils/ApiError";
 import prisma from "../../utils/prisma";
-import { getUserById } from "./user.get.services";
 
 export const deleteUserById = async (userId: string) => {
-    const user = await getUserById(userId);
-    if (!user) {
-      throw new ApiError(httpStatus.NOT_FOUND, "User not found");
-    }
   return await prisma.user.delete({
     where: {
       id: userId,
